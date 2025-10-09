7b2b86f4452096775ccee9d4f23a30928477c024
NicolasRstart <nicoruocco2006@gmail.com>
2025-10-09 14:28:47 +0000

    posible ultima upd

--- thirs tyr/src/main.jsx
+++ thirs tyr/src/main.jsx
@@ -4,6 +4,28 @@ import App from "./App.jsx";
 import "./index.css";
 import { CartProvider } from "./assets/CartContext";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC5mOqWD9-4b25CTnyK8azlOwSPCbVZIVo",
+  authDomain: "landing-nico-r.firebaseapp.com",
+  projectId: "landing-nico-r",
+  storageBucket: "landing-nico-r.firebasestorage.app",
+  messagingSenderId: "1069587971759",
+  appId: "1:1069587971759:web:d9cec99aacf07918851e5a",
+  measurementId: "G-J33YM4PM6F"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+getAnalytics(app);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CartProvider>
